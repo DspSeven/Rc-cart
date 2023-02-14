59713207c83d273a9410f6a8f868740e84456185
Your Name <you@example.com>
2023-02-14 03:40:06 +0000

    Rc-cart

---
+++ src/components/Cart/index.js
@@ -0,0 +1,30 @@
+import Header from '../Header'
+import CartListView from '../CartListView'
+import CartContext from '../../context/CartContext'
+import EmptyCartView from '../EmptyCartView'
+import './index.css'
+
+const Cart = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList} = value
+      const showEmptyCart = cartList.length === 0
+      return (
+        <>
+          <Header />
+          {showEmptyCart ? (
+            <EmptyCartView />
+          ) : (
+            <div className="cart-container">
+              <div className="cart-content-container">
+                <h1 className="cart-heading">My Cart</h1>
+                <CartListView />
+              </div>
+            </div>
+          )}
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+export default Cart
